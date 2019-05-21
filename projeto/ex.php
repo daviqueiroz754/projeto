8615ae78f3334ba9f35d465ff61718a1563c545a
daviqueiroz754 <34130756+daviqueiroz754@users.noreply.github.com>
2019-05-20 22:21:58 -0300

    Add files via upload

---
+++ projeto/ex.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>annn</title>
+</head>
+<body>
+
+<h1> aadoncaosidn </h1>
+
+	<?php 
+		$n = 4;
+		$no = "GUSTAVO";
+		echo $no;
+	 ?>
+
+</body>
+</html>
